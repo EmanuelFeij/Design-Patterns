06d2133bcb7cdb7b912f52acb5a7efeaa87d2bb7
emanuel <emanuel@localhost.localdomain>
2021-03-28 21:46:35 +0100

    Generator pattern added

---
+++ Generator/generator.go
@@ -0,0 +1,23 @@
+/* package main
+
+import "fmt"
+
+func Count(n int) chan int {
+	out := make(chan int)
+
+	go func() {
+		defer close(out)
+		for i := 0; i < n; i++ {
+			out <- i
+		}
+	}()
+
+	return out
+}
+
+func main() {
+	for i := range Count(200) {
+		fmt.Println(i)
+	}
+}
+*/
